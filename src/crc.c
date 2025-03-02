6d1c5b0266cba5efc19c31823cda329d6db0ccba
altehex <ivanronalddobsky@yandex.ru>
2025-03-02 14:27:25 +0500

    Removed unnecessary crc &= 0xFFFFFFFF

--- src/crc.c
+++ src/crc.c
@@ -17,7 +17,6 @@ __SBK_gen_crc(const uint32_t  *msg,
 		for (int b = 0; b < 32; ++b) {
 			x = (crc>>31) & 1;
 			crc <<= 1;
-			crc &= 0xFFFFFFFF;
 			if (x ^ (1 & (msg[i] >> (31-b))))
 				crc ^= 0x04C11DB7;
 		}
